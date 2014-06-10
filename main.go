2b8d80a5e3464142a7c290fd19c0d3c4a35b5828
Nanda Kishore <jnandakishore@zeomega.com>
2014-06-10 17:55:54 +0530

    CLI skeleton is in place

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World! Grit Binary.")
+	buildDirectory, _ := os.Getwd()
+	if len(os.Args) == 1 {
+		fmt.Print(errors.New("Usage: grit [option]. Where [option] could be one of boot/build/versions/shell\n"))
+		return
+	}
+	for _, subCommand := range os.Args {
+		if subCommand == "grit" {
+			continue
+		}
+		if subCommand == "boot" {
+			fmt.Println("Bootstrapping ", buildDirectory)
+			return
+		} else if subCommand == "build" {
+			fmt.Println("Building ", buildDirectory)
+			return
+		} else if subCommand == "versions" {
+			fmt.Println("Printing the versions of ", buildDirectory)
+			return
+		} else if subCommand == "shell" {
+			fmt.Println("Opening shell for ", buildDirectory)
+			return
+		} else {
+			fmt.Print(errors.New("Usage: grit [option]. Where [option] could be one of boot/build/versions/shell\n"))
+			return
+		}
+	}
 }
